20edc111c289efdd515e5144c70b6a794fb95674
Igor Dudek <igor.dudek@firma.interia.pl>
2025-05-30 17:02:34 +0200

    fix: remove dl issue

--- src/components/ui/organisms/DescriptionListSection/DescriptionListSection.tsx
+++ src/components/ui/organisms/DescriptionListSection/DescriptionListSection.tsx
@@ -26,15 +26,15 @@ const DescriptionListItem = ({ icon: Icon, link, namespace, index, animate }: TD
       className="cursor-pointer group relative pl-16 rounded-lg transition-colors"
     >
       <Link href={link} key={link} className="">
-        <dt className="font-semibold text-gray-900 dark:text-white">
+        <div className="font-semibold text-gray-900 dark:text-white">
           <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-accented-primary">
             <Icon aria-hidden="true" className="size-6 text-white" />
           </div>
           {t('title')}
-        </dt>
-        <dd className="text-base/7 text-gray-600 dark:text-gray-400 group-hover:text-accented-primary transition-colors">
+        </div>
+        <div className="text-base/7 text-gray-600 dark:text-gray-400 group-hover:text-accented-primary transition-colors">
           {t('caption')}
-        </dd>
+        </div>
         <button className="cursor-pointer absolute -bottom-7 right-0 flex items-center text-accented-primary lg:invisible">
           {t('button.read_more')} <FaRegPaperPlane className="ml-2" />
         </button>
@@ -54,14 +54,14 @@ const DescriptionListSection = ({ namespace }: TDescriptionListSectionProps) => 
   return (
     <section className="relative overflow-hidden transition-colors duration-300">
       <div className="mx-auto container py-6 lg:py-12 px-4 md:px-5">
-        <motion.dl
+        <motion.div
           ref={ref}
           className="mx-auto grid max-w-none grid-cols-1 gap-x-8 gap-y-10 md:max-w-4xl md:grid-cols-2 md:gap-y-16 lg:gap-x-32"
         >
           {items.map((data, index) => (
             <DescriptionListItem key={index} {...data} index={index} animate={isInView} />
           ))}
-        </motion.dl>
+        </motion.div>
       </div>
     </section>
   );
