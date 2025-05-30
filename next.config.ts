6f8752dbc77daaea7c45c902eecd6e76076a76f5
Igor Dudek <igor.dudek@firma.interia.pl>
2025-05-30 13:45:26 +0200

    feat: create CSP Headers

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,48 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' 
+    https://*.googletagmanager.com 
+    https://*.google-analytics.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data: blob: 
+    https://*.google-analytics.com 
+    https://*.googletagmanager.com;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  frame-src https://*.googletagmanager.com;
+  connect-src 'self' 
+    https://*.google-analytics.com 
+    https://*.analytics.google.com 
+    https://*.googletagmanager.com 
+    https://*.gstatic.com;
+    upgrade-insecure-requests;
+`;
+
 const nextConfig: NextConfig = {
   compress: true, // GZIP compression
   poweredByHeader: false,
   // output: 'standalone',
   experimental: {
     optimizePackageImports: ['@next/third-parties'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
   },
 };
 
