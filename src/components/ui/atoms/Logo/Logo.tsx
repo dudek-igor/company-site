535197beb2e5d4e4fcfde893442c8917d28d39a7
Igor Dudek <igor.dudek@firma.interia.pl>
2025-05-30 10:19:12 +0200

    fix: logo link

--- src/components/ui/atoms/Logo/Logo.tsx
+++ src/components/ui/atoms/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import { getLinkHrefViaNamespace } from '@/utils';
 
 const Logo = () => (
   <div className="min-w-[190px] flex justify-center items-center">
-    <Link className="text-nowrap text-3xl font-light text-accented-primary" href={getLinkHrefViaNamespace('CONTACT')}>
+    <Link className="text-nowrap text-3xl font-light text-accented-primary" href={getLinkHrefViaNamespace('HOME_PAGE')}>
       Hello Software
     </Link>
     {/* Add sr-only when create real one logo */}
